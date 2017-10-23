ec9ec58e3fe4585b74bb78b15d8a3f1309712490
Shipra Arora <shipraarora26@gmail.com>
2017-10-22 21:46:37 -0400

    attempting to make images work in production

--- bun-bun-bakery/src/ProductDetails.js
+++ bun-bun-bakery/src/ProductDetails.js
@@ -1,9 +1,47 @@
 import React, { Component } from 'react';
 import Header from './Header';
 
+import bacon_image from './images/bacon.jpg';
+import birthday_image from './images/birthday.jpg';
+import blackberry_image from './images/blackberry.jpg';
+import buttermilk_image from './images/buttermilk.jpg';
+import caramelPecan_image from './images/caramelPecan.jpg';
+import carrotCake_image from './images/carrotCake.jpg';
+import cranberry_image from './images/cranberry.jpg';
+import lemonLavender_image from './images/lemonLavender.jpg';
+import mapleApplePecan_image from './images/mapleApplePecan.jpg';
+import original_image from './images/original.jpg';
+import originalGlutenFree_image from './images/originalGlutenFree.jpg';
+import originalVegan_image from './images/originalVegan.jpg';
+import pumpkinSpice_image from './images/pumpkinSpice.jpg';
+import strawberryRhubarb_image from './images/strawberryRhubarb.jpg';
+import walnut_image from './images/walnut.jpg';
+
 import './ProductDetails.css';
 
 class ProductDetails extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      bacon_image: bacon_image,
+      birthday_image: birthday_image,
+      blackberry_image: blackberry_image,
+      buttermilk_image: buttermilk_image,
+      caramelPecan_image: caramelPecan_image,
+      carrotCake_image: carrotCake_image,
+      cranberry_image: cranberry_image,
+      lemonLavender_image: lemonLavender_image,
+      mapleApplePecan_image: mapleApplePecan_image,
+      original_image: original_image,
+      originalGlutenFree_image: originalGlutenFree_image,
+      originalVegan_image: originalVegan_image,
+      pumpkinSpice_image: pumpkinSpice_image,
+      strawberryRhubarb_image: strawberryRhubarb_image,
+      walnut_image: walnut_image
+    };
+  }
+
   addToCart(item) {
     var quantity
 
@@ -28,6 +66,8 @@ class ProductDetails extends Component {
   }
 
   render() {
+    var a = this.props.imagePath;
+
     return (
       <div className="ProductDetailsContainer">
         <Header displayPage="ProductMenu" />
@@ -36,7 +76,7 @@ class ProductDetails extends Component {
         <div className="productDes">
           <div className="flavorDes">
             <div className="leftDes">
-              <img className="flavorDesImg" src={this.props.imagePath} alt=""></img>
+              <img className="flavorDesImg" src={this.state[a]} alt=""></img>
               <span>$ {this.props.price}</span>
               <button onClick={() => this.addToCart(this.props)}>Add to Cart</button>
             </div>
